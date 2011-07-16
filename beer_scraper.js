c6892c11421977818b3bc028ae26fbb6203e0f5f
Kevin Ruffin <kruffin@gmail.com>
2011-07-16 14:01:33 -0400

    Added some extra information on the page structure.

--- beer_scraper.js
+++ beer_scraper.js
@@ -15,7 +15,58 @@ You should have received a copy of the GNU General Public License
 along with tmac_chrome_extension.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/*
+    The page looks like this:
+    <body>
+    <div>
+     <div>
+      <div>
+       <div>
+        <div>
+         <div>
+          <div>
+           <div>
+            <div>
+             Each div in here is a separate beer
+             <div>
+              The div inside here has a class of "beer-info"
+              <div class="beer-info">
+                 This is the one we want
+                 <div class="header">
+                  <div class="name"></div>
+                  <div class="format"></div>
+                  <div class="style"></div>
+                  <div class="abv"></div>
+                 </div>
+                 <div class="body">
+                  <div class="description">
+                  
+                  </div>
+                  <div class="data">
+                   <div class="brewery">
+                    <span class="value"></span>
+                   </div>
+                   <div class="origin">
+                    <span class="value"></span>
+                   </div>
+                  </div>
+                 </div>
+              </div>
+             </div>
+            </div>
+           </div>
+          </div>
+         </div>
+        </div>
+       </div>
+      </div>
+     </div>
+    </div>
+    </body>
+*/
 function findBeers() {
+	var beerDivs = document.getElementsByClassName('beer-info');
+	
 	return [{name: "Random", container: { bottle: true, draught: false }, style: "", alcohol: "", description: "", brewery: "", origin: "", picture: ""}];
 }
 
